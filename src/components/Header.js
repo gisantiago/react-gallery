d897352e1f36221953494ed73ff680fd98fc07a5
Gabriel Santiago <gisantiago@live.com>
2019-03-04 12:42:58 -0500

    Created all the components
    Header - Gallery - Nav - Search

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const Header = (props) => {
+    return (
+        <header className="App-header">
+            <title>React Gallery App</title>
+            <link href="../../css/index.css" rel="stylesheet"></link>
+        </header>
+    );
+}
+
+export default Header;
